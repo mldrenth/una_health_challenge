1dc24edffc28cf927985dfbac24ae1f2bd9f3653
Marijke Drenth <drenth.marijke@googlemail.com>
2022-03-18 12:26:38 +0200

    Shows Levels for user id

--- una_health_app/views.py
+++ una_health_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from una_health_app.models import *
 
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'una_health_app/index.html')
+    users = User.objects.all()
+    if request.method == "POST":
+        form_data = request.POST
+        user_id= form_data['user_id']
+        user = User.objects.get(user_id = user_id)
+        glucose_values = GlucoseValue.objects.filter(user_id = user.id)
+        return render(request, 'una_health_app/userdata.html', locals())
+    else:
+        return render(request, 'una_health_app/index.html', locals())
 
