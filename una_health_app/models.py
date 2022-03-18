958732a5520c8d436b3d6ab14aca3116dace504f
Marijke Drenth <drenth.marijke@googlemail.com>
2022-03-18 10:32:36 +0200

    Add models and tests

--- una_health_app/models.py
+++ una_health_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user_id
+
+class GlucoseValue(models.Model):
+    device = models.CharField(max_length=255)
+    serial_number = models.CharField(max_length=255)
+    time_stamp = models.DateTimeField()
+    recording_type = models.IntegerField()
+    glucose_history = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
